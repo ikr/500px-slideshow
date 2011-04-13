837e0ba777263e10ac382fc30bfba6297dd4b0f8
Ivan Krechetov <ivan.krechetov@gmail.com>
2011-04-13 18:03:23 +0200

    Extracted 500px-slideshow.js

---
+++ 500px-slideshow.js
@@ -0,0 +1,11 @@
+var ikr500px = function (jQuery) {
+    return {
+        thumbnailImgNodes: function () {
+            return jQuery("div.thumb > a.image > img");
+        },
+        
+        slideshow: function () {
+            
+        }
+    };
+};
--- tests.js
+++ tests.js
@@ -1,5 +1,12 @@
 module("Page photos extraction");
 
-test("thumbnailLinkNodes size is correct", function () {
-    strictEqual(ikr500px.thumbnailLinkNodes().size(), 20);
+test("thumbnailImgNodes size is correct", function () {
+    strictEqual(ikr500px($).thumbnailImgNodes().size(), 20);
+});
+
+test("thumbnailImgNodes first image source is correct", function () {
+    strictEqual(
+        ikr500px($).thumbnailImgNodes().first().attr("src"),
+        "http://photos.500px.com/484082/3"
+    );
 });
