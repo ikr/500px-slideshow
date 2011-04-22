92b2dd2e3d68238a23af787d8f45455b139eec21
Ivan Krechetov <ivan.krechetov@gmail.com>
2011-04-22 22:00:59 +0200

    Simplification

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,10 +1,11 @@
 if (typeof(ikr500px) === "undefined") {
     var link = document.createElement("link");
-    $(link).attr("rel", "stylesheet");
-    $(link).attr("href", "http://krechetov.net/~ikr/jquery-lightbox/css/lightbox.css");
-    $(link).attr("type", "text/css");
-    $(link).attr("media", "screen");
-    $("head").append(link);
+
+    $(link).attr({
+        "rel": "stylesheet",
+        "href": "http://krechetov.net/~ikr/jquery-lightbox/css/lightbox.css",
+        "type": "text/css"
+    });
 
     $.getScript("http://krechetov.net/~ikr/jquery-lightbox/jquery.lightbox.js", function () {
         $.getScript("http://krechetov.net/~ikr/500px-slideshow/500px-slideshow.js", function () {
