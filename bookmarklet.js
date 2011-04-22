a6e4ed48144538c36363aa36562625cbb3df88b2
Ivan Krechetov <ivan.krechetov@gmail.com>
2011-04-22 22:29:59 +0200

    Address the cross-browser issues

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,20 +1,22 @@
-if (typeof(ikr500px) === "undefined") {
-    var link = document.createElement("link");
+(function () {
+    if (typeof(ikr500px) === "undefined") {
+        var link = document.createElement("link");
 
-    $(link).attr({
-        "rel": "stylesheet",
-        "href": "http://krechetov.net/~ikr/jquery-lightbox/css/lightbox.css",
-        "type": "text/css"
-    });
-    
-    $("head").append(link);
-
-    $.getScript("http://krechetov.net/~ikr/jquery-lightbox/jquery.lightbox.js", function () {
-        $.getScript("http://krechetov.net/~ikr/500px-slideshow/500px-slideshow.js", function () {
-            ikr500px.slideshow();
+        $(link).attr({
+            "rel": "stylesheet",
+            "href": "http://krechetov.net/~ikr/jquery-lightbox/css/lightbox.css",
+            "type": "text/css"
         });
-    });
-}
-else {
-    ikr500px.slideshow();
-}
+    
+        $("head").append(link);
+
+        $.getScript("http://krechetov.net/~ikr/jquery-lightbox/jquery.lightbox.js", function () {
+            $.getScript("http://krechetov.net/~ikr/500px-slideshow/500px-slideshow.js", function () {
+                ikr500px.slideshow();
+            });
+        });
+    }
+    else {
+        ikr500px.slideshow();
+    }
+}());
