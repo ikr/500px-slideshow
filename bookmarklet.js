7670a5575b0766c67015bdc8809aaa2cdcc2964a
Ivan Krechetov <ivan.krechetov@gmail.com>
2011-04-22 21:48:17 +0200

    Avoid the nested quotes for javascript: URL

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,7 +1,10 @@
 if (typeof(ikr500px) === "undefined") {
-    $("head").append(
-        '<link rel="stylesheet" href="http://krechetov.net/~ikr/jquery-lightbox/css/lightbox.css" type="text/css" media="screen" />'
-    );
+    var link = document.createElement("link");
+    $(link).attr("rel", "stylesheet");
+    $(link).attr("href", "http://krechetov.net/~ikr/jquery-lightbox/css/lightbox.css");
+    $(link).attr("type", "text/css");
+    $(link).attr("media", "screen");
+    $("head").append(link);
 
     $.getScript("http://krechetov.net/~ikr/jquery-lightbox/jquery.lightbox.js", function () {
         $.getScript("http://krechetov.net/~ikr/500px-slideshow/500px-slideshow.js", function () {
