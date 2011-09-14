6ecc817100d8cc92f136f3d7e7fe12b7195a1cff
Ivan Krechetov <ivan.krechetov@gmail.com>
2011-09-14 11:23:38 +0200

    New full-size image names

--- 500px-slideshow.js
+++ 500px-slideshow.js
@@ -2,35 +2,35 @@ var ikr500px = {
     thumbnailImgNodes: function () {
         return jQuery("div.thumb > a.image > img");
     },
-    
+
     fullSizeImageUrl: function (thumbnailUrl) {
-        return thumbnailUrl.substr(0, thumbnailUrl.lastIndexOf("/")) + "/5";
+        return thumbnailUrl.substr(0, thumbnailUrl.lastIndexOf("/")) + "/4.jpg";
     },
-    
+
     populateLightboxContainer: function (prependTo) {
         jQuery("#ikr500px-lightbox-root").remove();
         jQuery(prependTo).prepend('<div id="ikr500px-lightbox-root" style="display: none"/>');
-        
+
         var that = this;
-        
+
         this.thumbnailImgNodes().each(function () {
             var url = that.fullSizeImageUrl(jQuery(this).attr("src"));
-            
+
             jQuery("#ikr500px-lightbox-root").append(
                 '<a href="' + url + '" rel="lightbox[ikr500px-slideshow]" class="ikr500px-lightbox-a">' + url + '</a>'
             );
         });
     },
-    
+
     slideshow: function () {
         this.populateLightboxContainer('body');
 
         jQuery(".ikr500px-lightbox-a").lightbox({
             fileLoadingImage: "http://krechetov.net/~ikr/jquery-lightbox/images/loading.gif",
             fileBottomNavCloseImage: "http://krechetov.net/~ikr/jquery-lightbox/images/closelabel.gif"
-            
+
         });
-        
+
         jQuery(".ikr500px-lightbox-a").first().trigger("click");
     }
 };
